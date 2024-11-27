5442fc8bca6eee68d5445d97788f9eace8af269c
JoseAusejo <joseantonioausejotoribio@gmail.com>
2024-11-27 16:59:49 -0500

    Commit inicial del proyecto Aplicación

---
+++ src/com/apli/radio/modelo/MisFavoritos.java
@@ -0,0 +1,13 @@
+package com.apli.radio.modelo;
+
+public class MisFavoritos {
+        public void adicione(Audio audio){
+            if(audio.getClasificacion() >= 8){
+                System.out.println(audio.getTitulo() + "Es uno de los favoritos del momento");
+            } else {
+                System.out.println(audio.getTitulo() + "También es uno de los favoritos");
+            }
+        }
+}
+
+
